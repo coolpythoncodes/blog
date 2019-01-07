53f39cc3a66f1aa75975b2f847ae6b496bb960d8
coolpythoncodes <chijiokeibe15@gmail.com>
2019-01-08 02:01:20 +1200

    added a detail view in views.py. This enables you see a blog post fully

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import DetailView,ListView
 
 from .models import BlogPost
 
@@ -7,3 +7,7 @@ from .models import BlogPost
 class BlogListView(ListView):
     model = BlogPost
     template_name = "home.html"
+
+class BlogDetailView(DetailView):
+    model = BlogPost
+    template_name = "post_detail.html"
