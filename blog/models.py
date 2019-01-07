99edb9c9c1b80b035504fc7eff8f926b474debac
coolpythoncodes <chijiokeibe15@gmail.com>
2019-01-08 03:45:55 +1200

    debugging

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -11,5 +12,8 @@ class BlogPost(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail',args=[str(self.id)])
 
 
