df313b6c0456df6ae7244a102cf3616a66496738
coolpythoncodes <chijiokeibe15@gmail.com>
2019-01-08 02:10:47 +1200

    configure url for BlogDetailView

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='home'),
+    path('post/<int:pk>',views.BlogDetailView.as_view(),name='post_detail'),
 ]
