a16334e788c1a673c92f389eea4d4f9894f4324a
coolpythoncodes <chijiokeibe15@gmail.com>
2019-01-07 23:20:17 +1200

    created a BlogListView to see the home page of the blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import BlogPost
 
 # Create your views here.
+
+class BlogListView(ListView):
+    model = BlogPost
+    template_name = "home.html"
