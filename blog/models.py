ed60cb0968ab5cd3e9756470b435d5fef8268070
coolpythoncodes <chijiokeibe15@gmail.com>
2019-01-08 01:37:44 +1200

    added datetimefield in models.py and modified the html files

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('auth.User',on_delete=models.CASCADE,)
     body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
