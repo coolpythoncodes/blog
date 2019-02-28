086a2257b4554862bbd3f36d98456c539b4526dc
coolpythoncodes <chijiokeibe15@gmail.com>
2019-03-01 01:30:39 +1200

    minor changes

--- blog/models.py
+++ blog/models.py
@@ -8,24 +8,28 @@ class PublishedPostManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status="Publish")
 
+
 class BlogPost(models.Model):
     POST_STATUS_CHOICES = (
-        ('Publish','Publish'),
+        ('Publish', 'Publish'),
         ('Draft', 'Draft'),
     )
     title = models.CharField(max_length=200)
-    author = models.ForeignKey('auth.User',on_delete=models.CASCADE,)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE,)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=7,choices=POST_STATUS_CHOICES,default='Draft')
+    status = models.CharField(max_length=7, choices=POST_STATUS_CHOICES, default='Draft')
+
+    objects = models.Manager()
+    published = PublishedPostManager()
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post_detail',args=[str(self.id)])
+        return reverse('post_detail', args=[str(self.id)])
 
     class Meta:
-        ordering = ('-date',) # show posts from latest published to the first published
+        ordering = ('-date',)  # show posts from latest published to the first published
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,17 @@
 from django.urls import reverse_lazy
-from django.views.generic import DetailView,ListView
+from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from .models import BlogPost
 
 # Create your views here.
 
+
 class BlogListView(ListView):
     queryset = BlogPost.published.all()  # retrieves only published posts
     model = BlogPost
     template_name = "home.html"
+
 
 class BlogDetailView(DetailView):
     model = BlogPost
@@ -21,10 +23,12 @@ class BlogCreateView(CreateView):
     fields = "__all__"
     template_name = "post_new.html"
 
+
 class BlogUpdateView(UpdateView):
     model = BlogPost
-    fields = ['title','body']
+    fields = ['title', 'body']
     template_name = "post_update.html"
+
 
 class BlogDeleteView(DeleteView):
     model = BlogPost
