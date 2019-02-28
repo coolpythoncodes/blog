396e468d6dc030047f076c610e1c4225f67aa3cd
coolpythoncodes <chijiokeibe15@gmail.com>
2019-03-01 01:48:52 +1200

    added a  model for comment

--- blog/models.py
+++ blog/models.py
@@ -33,3 +33,18 @@ class BlogPost(models.Model):
         ordering = ('-date',)  # show posts from latest published to the first published
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(
+        BlogPost,
+        on_delete=models.CASCADE,
+        related_name='comments')
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created',)  # show comments from latest  to the first
+
+    def __str__(self):
+        return 'comment by {} on {}'.format(self.name, self.post)
