ba144beab9bfd217df277468c09c27805781e2e2
coolpythoncodes <chijiokeibe15@gmail.com>
2019-03-02 01:43:14 +1200

    registered models in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import BlogPost
+from .models import BlogPost, Comment
 
 # Register your models here.
-admin.site.register(BlogPost)
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title','author','date','status')
+
+@admin.register(Comment)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name','email','post')
+    ordering = ('-created',)
+
+
+
