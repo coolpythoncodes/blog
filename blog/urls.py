4e0873157c5d008be88a597033567946bfb7021c
coolpythoncodes <chijiokeibe15@gmail.com>
2019-03-05 09:18:17 +1200

    added slug to url path

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.BlogListView.as_view(), name='home'),
-    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
-    #path('post/<int:pk>', views.BlogDetailView.as_view(), name='post_detail'),
-    path('post/new', views.BlogCreateView.as_view(), name='post_new'),
+    path('', views.post_list, name='home'),
+    # path('', views.BlogListView.as_view(), name='home'),
+    path('<slug:post>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/edit', views.BlogUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete', views.BlogDeleteView.as_view(), name='post_delete')
 ]
