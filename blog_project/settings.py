b234468810b31f8804fe500a6d45cd5efdaddea0
coolpythoncodes <chijiokeibe15@gmail.com>
2019-03-04 21:15:11 +1200

    Added Crispy Form

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #Local Apps
+
+    # Local Apps
     'blog',
+
+    # Third party apps
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
