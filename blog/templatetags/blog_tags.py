9cc943705f63da02b181f8b6d1e4dc42069746dc
coolpythoncodes <chijiokeibe15@gmail.com>
2019-03-05 12:38:21 +1200

    custom tag to show most commented posts

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,9 +1,10 @@
 from django import template
+from django.db.models import Count
 from ..models import BlogPost
 
 register = template.Library()
 
-
+# custom tag to get the most recent published post
 @register.inclusion_tag('recent_post.html')
 def show_recent_post(count=3):
     recent_post = BlogPost.published.all()[:count]  # Retrieves recent blog post according to the value of count
@@ -12,3 +13,12 @@ def show_recent_post(count=3):
         'recent_post': recent_post,
         
     }
+
+
+# custom tag to get the most commented post
+@register.simple_tag
+def most_commented(count=5):
+    return BlogPost.published.annotate(
+        total_comments = Count('comments')
+    ).order_by('-total_comments')[:count]
+    
