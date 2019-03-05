d98b7980b3c5190cc860930bb3f981fb0a5a4625
coolpythoncodes <chijiokeibe15@gmail.com>
2019-03-05 12:36:50 +1200

    installed the sitemap app

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # sitemaps
+    'django.contrib.sitemaps',
 
     # Local Apps
     'blog',
